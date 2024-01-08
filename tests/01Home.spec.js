f2b3cbbe3e39e960b6f84eab18571b1c6129033d
jennyxblessing <jenny.blessing91@gmail.com>
2024-01-07 21:39:18 -0800

    Adding and editing tests

--- tests/01Home.spec.js
+++ tests/01Home.spec.js
@@ -1,14 +1,16 @@
 const {test, expect} = require('@playwright/test')
 
-test ('Navigate to website and check that heading is as expected', async ({page}) =>{
+test ('Navigate to website and log in', async ({page}) =>{
     
-    await page.goto ('https://automationintesting.online/');
-    await expect (page).toHaveTitle('Restful-booker-platform demo'); 
+    await page.goto('https://www.saucedemo.com/v1/');
+    await expect.soft(page).toHaveTitle(/Swag Labs/); 
 
-})
+    await page.click("#login-button");
+    await expect.soft(page.getByText('Epic Sadface')).toBeVisible();
 
-test ('Select Book This Room button', async ({page}) =>{
+    await page.locator("#user-name").fill("standard_user");
+    await page.locator("#password").fill("secret_sauce");
+    await page.keyboard.press('Enter');
+    await expect.soft(page.getByText('Products')).toBeVisible();
 
-    await page.click('');
-
-})
+ })
---
+++ tests/02Products.spec.js
@@ -0,0 +1,6 @@
+const {test, expect} = require('@playwright/test')
+
+test ('Add products to cart', async ({page}) =>{
+    
+
+ })
